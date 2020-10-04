88736b056ede134014f294031722fe4f8bbc5767
Giancarlo A <gian.al08@hotmail.com>
2020-10-03 20:52:50 -0500

    2020-10-03 desde trabajo

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require('fs');
 const axios = require('axios');
-const shelljs = require('shelljs');
+const shelljs = require('shelljs');//no se lo usa
 const cors = require('cors');
 const config = require('./config.json');
 const { Client } = require('whatsapp-web.js');
@@ -12,16 +12,13 @@ if (fs.existsSync(SESSION_FILE_PATH)) {
     sessionCfg = require(SESSION_FILE_PATH);
 }
 process.title = "whatsapp-node-api";
-global.client = new Client({ puppeteer: { headless: true , args:['--no-sandbox','--disable-setuid-sandbox','--unhandled-rejections=strict'] }, session: sessionCfg});
+global.client = new Client({ puppeteer: { headless: true , args:['--no-sandbox','--disable-setuid-sandbox','--unhandled-rejections=strict'] }, session: sessionCfg, executablePath: 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'});
 global.authed = false;
 const app = express();
-
-
 const port = process.env.PORT || config.port;
-app.use(cors());
-app.use(express.json());
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());//para las los frontend que trabajan con cors (opcional)
+app.use(bodyParser.json({limit: '50mb'}));//el limite 
+app.use(bodyParser.urlencoded({ limit: '50mb', 'extended': 'true', parameterLimit: 50000  }));//el limite 
 
 client.on('qr', qr => {
     fs.writeFileSync('./components/last.qr',qr);
@@ -63,16 +60,17 @@ const groupRoute = require('./components/group');
 const authRoute = require('./components/auth');
 const contactRoute = require('./components/contact');
 
-app.use(function(req,res,next){
+app.use(function(req,res,next){//función que atrapa la peticion hhtp e imprime en consola
     console.log(req.method + ' : ' + req.path);
     next();
 });
+/*Los componentes que se usan con su respectiva rutas o rounting*/
+app.use('/auth',authRoute);
 app.use('/chat',chatRoute);
 app.use('/group',groupRoute);
-app.use('/auth',authRoute);
 app.use('/contact',contactRoute);
 
-app.listen(port, () => {
-    //mi propio redme http://localhost:5009/apiW/auth/getqr
+app.listen(port, () => {//para imprimir en consola el puerto que esta en la configuración
+    console.log(`si es primera vez que ingresa: http://localhost:${port}/auth/getqr`)
     console.log(`Run: http://127.0.0.1:${port}`);
 });
--- components/chatting.js
+++ components/chatting.js
@@ -1,176 +1,276 @@
 const router = require('express').Router();
-const { MessageMedia, Location } = require("whatsapp-web.js");
+const {
+    MessageMedia,
+    Location
+} = require("whatsapp-web.js");
 const request = require('request')
 const vuri = require('valid-url');
 const fs = require('fs');
 
 const mediadownloader = (url, path, callback) => {
     request.head(url, (err, res, body) => {
-      request(url)
-        .pipe(fs.createWriteStream(path))
-        .on('close', callback)
+        request(url)
+            .pipe(fs.createWriteStream(path))
+            .on('close', callback)
     })
-  }
+}
 
-router.post('/sendmessage/:phone', async (req,res) => {
-    let phone = req.params.phone;
-    let message = req.body.message;
+router.post('/sendmessage/:phone', async (req, res) => {//método para enviar un mensaje de texto
+    let phone = req.params.phone; //campo obligatorio
+    let message = req.body.message; //campo obligatorio
 
-    if(phone==undefined||message==undefined){
-        res.send({status:"error",message:"please enter valid phone and message"})
-    }else{
-        client.sendMessage(phone+'@c.us',message).then((response)=>{
-            if(response.id.fromMe){
-                res.send({status:'success',message:'Message successfully sent to '+phone})
+    if (phone == undefined || message == undefined) {
+        res.send({
+            status: "error",
+            message: "please enter valid phone and message"
+        })
+    } else {
+        client.sendMessage(phone + '@c.us', message).then((response) => {
+            if (response.id.fromMe) {
+                res.send({
+                    status: 'success',
+                    message: 'Message successfully sent to ' + phone
+                })
             }
         });
     }
 });
 
-router.post('/sendimage/:phone', async (req,res) => {
+router.post('/sendimage/:phone', async (req, res) => {//método para enviar un imagen 
     var base64regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
+    let phone = req.params.phone; //campos obligatorio
+    let image = req.body.media; //campo obligatorio
+    let caption = req.body.caption; // campo opcional
+    let type = req.body.type; //campo obligatorio
 
-    let phone = req.params.phone;
-    let image = req.body.image;
-    let caption = req.body.caption;
-
-    if(phone==undefined||image==undefined){
-        res.send({status:"error",message:"please enter valid phone and base64/url of image"})
-    }else{
-        if(base64regex.test(image)){
-            let media = new MessageMedia('image/png',image);
-            client.sendMessage(phone+'@c.us',media,{caption:caption||""}).then((response)=>{
-                if(response.id.fromMe){
-                    res.send({status:'success',message:'MediaMessage successfully sent to '+phone})
+    if (phone == undefined || image == undefined) {
+        res.send({
+            status: "error",
+            message: "please enter valid phone and base64/url of image"
+        })
+    } else {
+        if (base64regex.test(image)) {//comprueba si es que el campo image que contiene la base64 es correcta
+            let media = new MessageMedia(type.tostring(), image); //el messageMedia recive 3 parametros: el mimetype(en este caso image/jpeg),la base 64, y el parametro opcional nombre 
+            client.sendMessage(phone + '@c.us', media, {
+                caption: caption || ""
+            }).then((response) => {
+                if (response.id.fromMe) {
+                    res.send({
+                        status: 'success',
+                        message: 'MediaMessage successfully sent to ' + phone
+                    })
                 }
             });
-        }else if(vuri.isWebUri(image)){
-            if (!fs.existsSync('./temp')){
+        } else if (vuri.isWebUri(image)) {
+            if (!fs.existsSync('./temp')) {
                 await fs.mkdirSync('./temp');
             }
             var path = './temp/' + image.split("/").slice(-1)[0]
-            mediadownloader(image,path,()=>{
+            mediadownloader(image, path, () => {
                 let media = MessageMedia.fromFilePath(path);
-                client.sendMessage(phone+'@c.us',media,{caption:caption||""}).then((response)=>{
-                    if(response.id.fromMe){
-                        res.send({status:'success',message:'MediaMessage successfully sent to '+phone})
+                client.sendMessage(phone + '@c.us', media, {
+                    caption: caption || ""
+                }).then((response) => {
+                    if (response.id.fromMe) {
+                        res.send({
+                            status: 'success',
+                            message: 'MediaMessage successfully sent to ' + phone
+                        })
                     }
                 });
             })
-        }else{
-            res.send({status:'error',message:'Invalid URL/Base64 Encoded Media'})
+        } else {
+            res.send({
+                status: 'error',
+                message: 'Invalid URL/Base64 Encoded Media'
+            })
         }
     }
 });
 
-router.post('/sendpdf/:phone', async (req,res) => {
+router.post('/sendfile/:phone', async (req, res) => {//método para enviar un pdf doc, excel 
     var base64regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
+    let phone = req.params.phone;//campo obligatorio
+    let doc = req.body.media;// campo obligatorio
+    let type = req.body.type; //campo obligatorio
+    let filename = req.body.title; //campo opcional pero recomendable
 
-    let phone = req.params.phone;
-    let doc = req.body.doc;
-
-    if(phone==undefined||doc==undefined){
-        res.send({status:"error",message:"please enter valid phone and base64/url of image"})
-    }else{
-        if(base64regex.test(doc)){
-            let media = new MessageMedia('application/pdf',doc);
-            client.sendMessage(phone+'@c.us',media).then((response)=>{
-                if(response.id.fromMe){
-                    res.send({status:'success',message:'MediaMessage successfully sent to '+phone})
+    if (phone == undefined || doc == undefined) {
+        res.send({
+            status: "error",
+            message: "please enter valid phone and base64/url of image"
+        })
+    } else {
+        if (base64regex.test(doc)) {//comprueba si es que el campo image que contiene la base64 es correcta
+            let media = new MessageMedia(type, doc, filename);//el messageMedia recive 3 parametros: el mimetype(en este caso applicacion/pdf),la base 64, y el nombre del archivo
+            client.sendMessage(phone + '@c.us', media).then((response) => {
+                if (response.id.fromMe) {
+                    res.send({
+                        status: 'success',
+                        message: 'MediaMessage successfully sent to ' + phone
+                    })
                 }
             });
-        }else if(vuri.isWebUri(image)){
-            if (!fs.existsSync('./temp')){
+        } else if (vuri.isWebUri(doc)) {
+            if (!fs.existsSync('./temp')) {
                 await fs.mkdirSync('./temp');
             }
-            var path = './temp/' + image.split("/").slice(-1)[0]
-            mediadownloader(image,path,()=>{
+            var path = './temp/' + doc.split("/").slice(-1)[0]
+            mediadownloader(doc, path, () => {
                 let media = MessageMedia.fromFilePath(path);
-                client.sendMessage(phone+'@c.us',media,{caption:caption||""}).then((response)=>{
-                    if(response.id.fromMe){
-                        res.send({status:'success',message:'MediaMessage successfully sent to '+phone})
+                client.sendMessage(phone + '@c.us', media).then((response) => {
+                    if (response.id.fromMe) {
+                        res.send({
+                            status: 'success',
+                            message: 'MediaMessage successfully sent to ' + phone
+                        })
                     }
                 });
             })
-        }else{
-            res.send({status:'error',message:'Invalid URL/Base64 Encoded Media'})
+        } else {
+            res.send({
+                status: 'error',
+                message: 'Invalid URL/Base64 Encoded Media'
+            })
         }
     }
 });
 
-router.post('/sendvideo/:phone', async (req,res) => {
+router.post('/sendmedia/:phone', async (req, res) => {//método para enviar imagenes, docuemtos, audios
     var base64regex = /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
+    let phone = req.params.phone;// campo obligatorio
+    let data = req.body.media;//campo obligatorio
+    let caption = req.body.caption;// campo opcional
+    let type = req.body.type;// campo obligatorio
+    let filename = req.body.title;// campo opcional
 
-    let phone = req.params.phone;
-    let video = req.body.video;
-    let caption = req.body.caption;
 
-    if(phone==undefined||video==undefined){
-        res.send({status:"error",message:"please enter valid phone and base64/url of video"})
-    }else{
-        if(base64regex.test(video)){
-            let media = new MessageMedia('video/mp4',video);
-            client.sendMessage(phone+'@c.us',media,{caption:caption||""}).then((response)=>{
-                if(response.id.fromMe){
-                    res.send({status:'success',message:'MediaMessage successfully sent to '+phone})
-                }
-            });
-        }else if(vuri.isWebUri(video)){
-            if (!fs.existsSync('./temp')){
-                await fs.mkdirSync('./temp');
-            }
-            var path = './temp/' + video.split("/").slice(-1)[0]
-            mediadownloader(video,path,()=>{
-                let media = MessageMedia.fromFilePath(path);
-                client.sendMessage(phone+'@c.us',media,{caption:caption||""}).then((response)=>{
-                    if(response.id.fromMe){
-                        res.send({status:'success',message:'MediaMessage successfully sent to '+phone})
+    if (phone == undefined || data == undefined) {
+        res.send({
+            status: "error",
+            message: "please enter valid phone and base64/url"
+        })
+    } else {
+        if (base64regex.test(data)) {//comprueba si es que el campo image que contiene la base64 es correcta
+            let media = new MessageMedia(type, data, filename);//el messageMedia recive 3 parametros: el mimetype(applicacion o image o audio),la base 64, y el nombre del archivo(opcional)
+            if (type.indexOf("application") >= 0) {//si en la cadena type se encuentra la palabra application pasa por el if caso contrario pasa por el else debido a que es una image o audio
+                client.sendMessage(phone + '@c.us', media).then((response) => {
+                    if (response.id.fromMe) {
+                        res.send({
+                            status: 'success',
+                            message: 'MediaMessage successfully sent to ' + phone
+                        })
                     }
                 });
+            } else {
+                client.sendMessage(phone + '@c.us', media, {caption: caption || "",sendAudioAsVoice: true}).then((response) => {
+                    if (response.id.fromMe) {
+                        res.send({
+                            status: 'success',
+                            message: 'MediaMessage successfully sent to ' + phone
+                        })
+                    }
+                }).catch(error => {
+                    console.log('caught', error.message);
+                });
+            }
+        } else if (vuri.isWebUri(data)) {
+            if (!fs.existsSync('./temp')) {
+                await fs.mkdirSync('./temp');
+            }
+            var path = './temp/' + data.split("/").slice(-1)[0]
+            mediadownloader(data, path, () => {
+                let media = MessageMedia.fromFilePath(path);
+                if (type.indexOf("application") >= 0) {
+                    client.sendMessage(phone + '@c.us', media).then((response) => {
+                        if (response.id.fromMe) {
+                            res.send({
+                                status: 'success',
+                                message: 'MediaMessage successfully sent to ' + phone
+                            })
+                        }
+                    });
+                } else {
+                    client.sendMessage(phone + '@c.us', media, {
+                        caption: caption || "",
+                        sendAudioAsVoice: true
+                    }).then((response) => {
+                        if (response.id.fromMe) {
+                            res.send({
+                                status: 'success',
+                                message: 'MediaMessage successfully sent to ' + phone
+                            })
+                        }
+                    });
+                }
             })
-        }else{
-            res.send({status:'error',message:'Invalid URL/Base64 Encoded Media'})
+        } else {
+            res.send({
+                status: 'error',
+                message: 'Invalid URL/Base64 Encoded Media'
+            })
         }
     }
 });
 
-router.post('/sendlocation/:phone', async (req,res) => {
+router.post('/sendlocation/:phone', async (req, res) => {
     let phone = req.params.phone;
     let latitude = req.body.latitude;
     let longitude = req.body.longitude;
     let desc = req.body.description;
 
-    if(phone==undefined||latitude==undefined||longitude==undefined){
-        res.send({status:"error",message:"please enter valid phone, latitude and longitude"})
-    }else{
-        let loc = new Location(latitude,longitude,desc||"");
-        client.sendMessage(phone+'@c.us',loc).then((response)=>{
-            if(response.id.fromMe){
-                res.send({status:'success',message:'Location successfully sent to '+phone})
+    if (phone == undefined || latitude == undefined || longitude == undefined) {
+        res.send({
+            status: "error",
+            message: "please enter valid phone, latitude and longitude"
+        })
+    } else {
+        let loc = new Location(latitude, longitude, desc || "");
+        client.sendMessage(phone + '@c.us', loc).then((response) => {
+            if (response.id.fromMe) {
+                res.send({
+                    status: 'success',
+                    message: 'Location successfully sent to ' + phone
+                })
             }
         });
     }
 });
 
-router.get('/getchatbyid/:phone', async (req,res) => {
+router.get('/getchatbyid/:phone', async (req, res) => {
     let phone = req.params.phone;
-    if(phone==undefined){
-        res.send({status:"error",message:"please enter valid phone number"});
-    }else{
-        client.getChatById(phone+"@c.us").then((chat) => {
-            res.send({ status:"success", message: chat});
+    if (phone == undefined) {
+        res.send({
+            status: "error",
+            message: "please enter valid phone number"
+        });
+    } else {
+        client.getChatById(phone + "@c.us").then((chat) => {
+            res.send({
+                status: "success",
+                message: chat
+            });
         }).catch(() => {
             console.error("getchaterror")
-            res.send({status:"error",message:"getchaterror"})
+            res.send({
+                status: "error",
+                message: "getchaterror"
+            })
         })
     }
 });
 
-router.get('/getchats', async (req,res) => {
+router.get('/getchats', async (req, res) => {
     client.getChats().then((chats) => {
-        res.send({ status:"success", message: chats});
+        res.send({
+            status: "success",
+            message: chats
+        });
     }).catch(() => {
-        res.send({status:"error",message:"getchatserror"})
+        res.send({
+            status: "error",
+            message: "getchatserror"
+        })
     })
 });
 
